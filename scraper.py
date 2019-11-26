a3548c39e8ea46cda7f52adfa51c0340c9d46ed9
Roi262 <roi262@gmail.com>
2019-11-26 17:40:31 +0200

    works with deepcopy

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
-# import simplejson as json
 from pprint import pprint
+from copy import deepcopy
 import math
 import time
 import itemPageScrape
@@ -37,21 +37,20 @@ project_urls = [child.get_attribute('href') for child in child_nodes]
 ID = 0
 record_jsons = []
 for project_url in project_urls:
-    project_data = itemPageScrape.crawlPage(url=project_url, ID=ID) 
-    # a=1
-    record_jsons.append(json.dumps(itemPageScrape.crawlPage(project_url, ID)))
-    # record[ID] = (project_data)
+    project_data = deepcopy(itemPageScrape.crawlPage(url=project_url, ID=ID))
+    record_jsons.append(project_data)
+    # record_jsons.append(json.dumps(itemPageScrape.crawlPage(project_url, ID)))
     # time.sleep(5)
     ID += 1
 
-record = []
-for js in record_jsons:
-    record.append(json.loads(js))
+# record = []
+# for js in record_jsons:
+#     record.append(json.loads(js))
 
 # record = [json.loads(elem) for elem in record_jsons]
 
 # create and print final JSON to file
-records = {"record": record}
+records = {"record": record_jsons}
 final_dict = {"records": records}
 with open('output.txt', 'wt') as out:
     pprint(final_dict, stream=out)
